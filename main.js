445726de7bd3afc2846d04dabaa5ee27bf573b31
Debian <admin@ip-172-26-7-59.ec2.internal>
2021-07-23 06:43:22 +0000

    Fixed

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function main(){
 
     const list = document.querySelector("ul");
     list.appendChild(createProject("30DaysOfJavaScript: Day 23 - Project 1", "code/day23eventlistners/index.html"));
-    list.appendChild(createProject("30DaysOfJavaScript: Day 23 - Project 1A","code/day23eventlistners2/index.html"));
+    list.appendChild(createProject("30DaysOfJavaScript: Day 23 - Project 1A","code/day23eventlistners2/day23eventlistners2/index.html"));
     list.appendChild(createProject("30DaysOfJavaScript: Day 24 - Project 1", "code/day24solarsystem/day24solarsystem/index.html"));
 }
 
