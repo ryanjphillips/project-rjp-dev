d011830d1f7262412526863e4b8012eab584e665
Debian <admin@ip-172-26-7-59.ec2.internal>
2021-07-23 06:05:08 +0000

    Added Projects

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 function main(){
 
     const list = document.querySelector("ul");
-    list.appendChild(createProject("30DaysOfJavaScript: Day 23 - Project 1A", "code/day23eventlistners/index.html"));
+    list.appendChild(createProject("30DaysOfJavaScript: Day 23 - Project 1", "code/day23eventlistners/index.html"));
+    list.appendChild(createProject("30DaysOfJavaScript: Day 23 - Project 1A","code/day23eventlistners2/index.html"));
+    list.appendChild(createProject("30DaysOfJavaScript: Day 24 - Project 1", "code/day24solarsystem/day24solarsystem/index.html"));
 }
+
 
 main();
