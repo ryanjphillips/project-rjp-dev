98beba0488e05acf01722bfd31c717c0eb9012c2
Debian <admin@ip-172-26-7-59.ec2.internal>
2021-07-24 21:06:36 +0000

    Fixed Typos

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function main(){
     list.appendChild(createProject("30DaysOfJavaScript: Day 23 - Project 1", "code/day23eventlistners/index.html"));
     list.appendChild(createProject("30DaysOfJavaScript: Day 23 - Project 1A","code/day23eventlistners2/day23eventlistners2/index.html"));
     list.appendChild(createProject("30DaysOfJavaScript: Day 24 - Project 1", "code/day24solarsystem/day24solarsystem/index.html"));
-    list.appendChild(createProject("30DaysOfJavaScript: Day 26 - Project 1", "code/day25worldcountries/day25worldcountries//index.html"));
+    list.appendChild(createProject("30DaysOfJavaScript: Day 25 - Project 1", "code/day25worldcountries/day25worldcountries/index.html"));
 }
 
 
