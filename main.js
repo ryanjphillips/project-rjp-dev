a7b83db49c92b63b22fa4eea012da15ed2cf7db2
Ryan Phillips <ryan.phillips.j@gmail.com>
2021-08-01 02:47:36 -0500

    Added Final Project

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ function main(){
     list.appendChild(createProject("30DaysOfJavaScript: Day 23 - Project 1A","code/day23eventlistners2/day23eventlistners2/index.html"));
     list.appendChild(createProject("30DaysOfJavaScript: Day 24 - Project 1", "code/day24solarsystem/day24solarsystem/index.html"));
     list.appendChild(createProject("30DaysOfJavaScript: Day 25 - Project 1", "code/day25worldcountries/day25worldcountries/index.html"));
+    list.appendChild(createProject("30DaysOfJavaScript Final Project: Country Coupling", "code/countrycoupling/countrycoupling/homepage/index.html"));
 }
 
 
