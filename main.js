6b2aea8ae71c8b6fda1867aaf49d0ac3481b08ec
Ryan Phillips <ryan.phillips.j@gmail.com>
2021-08-08 15:18:26 -0500

    Fixed Merge Issue

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ function main(){
     list.appendChild(createProject("30DaysOfJavaScript: Day 24 - Project 1", "code/day24solarsystem/day24solarsystem/index.html"));
     list.appendChild(createProject("30DaysOfJavaScript: Day 25 - Project 1", "code/day25worldcountries/day25worldcountries/index.html"));
     list.appendChild(createProject("30DaysOfJavaScript Final Project: Country Coupling", "code/countrycoupling/countrycoupling/homepage/index.html"));
-    list.appendChild(createProject("Poke-Scope", "code/pokescope/poke-scope/public/index.html"));
 }
 
 
